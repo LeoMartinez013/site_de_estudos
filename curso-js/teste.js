53e3874d2232bd7716c513f249917df359af0ffa
LeoMartinez013 <105740782+LeoMartinez013@users.noreply.github.com>
2022-06-10 14:13:50 -0300

    att 14:13 10/06
    troca de pasta

---
+++ curso-js/teste.js
@@ -0,0 +1,8 @@
+var vel = 89
+if(vel >=80){
+    console.log(`Velocidade: ${vel}!!`)
+    console.log(`Você está multado`)
+}else{
+    console.log(`Velocidade: ${vel}.`)
+}
+console.log('testando')
